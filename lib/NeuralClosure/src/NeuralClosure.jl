6ffe13d1c980c6cc2791545c5e5de85cd01a67a5
Syver Døving Agdestein <syverda@gmail.com>
2024-05-05 14:18:42 +0200

    Move files

---
+++ lib/NeuralClosure/src/NeuralClosure.jl
@@ -0,0 +1,35 @@
+"""
+Neural closure modelling tools.
+"""
+module NeuralClosure
+
+using CUDA
+using ComponentArrays: ComponentArray
+using IncompressibleNavierStokes
+using IncompressibleNavierStokes: Dimension, momentum!, apply_bc_u!, project!
+using KernelAbstractions
+using Lux
+using NNlib
+using Observables
+using Random
+using Tullio
+using Zygote
+
+include("closure.jl")
+include("cnn.jl")
+include("fno.jl")
+include("training.jl")
+include("filter.jl")
+include("create_les_data.jl")
+
+export smagorinsky_closure
+export cnn, fno, FourierLayer
+export train
+export mean_squared_error, create_relerr_prior, create_relerr_post
+export create_loss_prior, create_loss_post
+export create_dataloader_prior, create_dataloader_post
+export create_callback, create_les_data, create_io_arrays
+export wrappedclosure
+export FaceAverage, VolumeAverage, reconstruct, reconstruct!
+
+end
